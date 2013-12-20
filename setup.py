24eb71b046b7e29d2af3d1479c1c4d3d1f2c1bcd
Will Daly <will@edx.org>
2013-12-20 13:07:47 -0500

    Added MANIFEST file; updated setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@
 
 from setuptools import setup
 
-VERSION = '0.0.2'
+VERSION = '0.0.3'
 DESCRIPTION = 'UI-level acceptance test framework'
 REQUIREMENTS = [
     line.strip() for line in open("requirements.txt").readlines()
