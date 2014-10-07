d2ad810f62c221c15daffe580535dbefb4cee799
Jesse Zoldak <zoldak@edx.org>
2014-10-07 15:11:27 -0400

    Bump version in docs and add API docs for performance classes

--- docs/conf.py
+++ docs/conf.py
@@ -38,9 +38,9 @@ copyright = u'2013, EdX'
 # built documents.
 #
 # The short X.Y version.
-version = '0.3.0'
+version = '0.3.2'
 # The full version, including alpha/beta/rc tags.
-release = '0.3.0'
+release = '0.3.2'
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
