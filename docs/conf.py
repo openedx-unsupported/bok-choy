1a3d07f1a771e796aaa6d54f0bc15ef2ab5e52cc
Jesse Zoldak <zoldak@edx.org>
2015-03-31 13:45:09 -0400

    Version 0.3.3

--- docs/conf.py
+++ docs/conf.py
@@ -31,16 +31,16 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'bok-choy'
-copyright = u'2013, EdX'
+copyright = u'2015, EdX'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
 # built documents.
 #
 # The short X.Y version.
-version = '0.3.2'
+version = '0.3.3'
 # The full version, including alpha/beta/rc tags.
-release = '0.3.2'
+release = '0.3.3'
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
