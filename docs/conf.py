c03448ca2255452693cd873deed11a3a551e6a76
Ben Patterson <bpatterson@edx.org>
2015-06-26 10:53:08 -0400

    Version 0.4.2

--- docs/conf.py
+++ docs/conf.py
@@ -38,9 +38,9 @@ copyright = u'2015, EdX'
 # built documents.
 #
 # The short X.Y version.
-version = '0.4.1'
+version = '0.4.2'
 # The full version, including alpha/beta/rc tags.
-release = '0.4.1'
+release = '0.4.2'
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
