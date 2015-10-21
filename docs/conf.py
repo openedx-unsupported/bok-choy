f934f65516745d2b6c69bff3c034ab9311c2f7df
Christine Lytwynec <clytwynec@edx.org>
2015-10-21 15:21:21 -0400

    v0.4.7

--- docs/conf.py
+++ docs/conf.py
@@ -38,9 +38,9 @@ copyright = u'2015, EdX'
 # built documents.
 #
 # The short X.Y version.
-version = '0.4.6'
+version = '0.4.7'
 # The full version, including alpha/beta/rc tags.
-release = '0.4.6'
+release = '0.4.7'
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@
 
 from setuptools import setup
 
-VERSION = '0.4.6'
+VERSION = '0.4.7'
 DESCRIPTION = 'UI-level acceptance test framework'
 REQUIREMENTS = [
     line.strip() for line in open("requirements.txt").readlines()
